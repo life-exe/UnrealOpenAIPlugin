40a0c73368be5278f6637969ff0b3e4b007f359d
life-exe <lifeexe.art@gmail.com>
2023-11-04 21:48:20 +0200

    Add setup auth files bat script

---
+++ setup_auth_files.bat
@@ -0,0 +1,5 @@
+:: Copyright LifeEXE. All Rights Reserved.
+@echo off
+
+copy "%~dp0\templates\OpenAIAuth.ini" "%~dp0..\..\OpenAIAuth.ini"
+copy "%~dp0\templates\OnlineServicesAuth.ini" "%~dp0..\..\OnlineServicesAuth.ini"
