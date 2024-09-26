527d8d0ef9d39b131af1b52226ceefc8f62d1d79
Life EXE <lifeexe.art@gmail.com>
2024-09-26 20:49:08 +0300

    Update clean intermediate script

--- clean_intermediate_files.bat
+++ clean_intermediate_files.bat
@@ -1,7 +1,7 @@
 :: Copyright LifeEXE. All Rights Reserved.
 @echo off
 
-set dirsToRemove=Intermediate Binaries Documentation
+set dirsToRemove=Intermediate Binaries Documentation Saved Build
 
 FOR %%f in (%dirsToRemove%) do (
     rmdir /s /q %%f
