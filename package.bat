11b663f440f389e6e690425dc07e25baf27813bb
life-exe <lifeexe.art@gmail.com>
2025-06-05 20:36:19 +0300

    Update package.bat

--- package.bat
+++ package.bat
@@ -1,17 +1,17 @@
 @echo off
 
-set "UATPath=C:\Program Files\Epic Games\UE_5.5\Engine\Build\BatchFiles\RunUAT.bat"
+set "UATPath=C:\Program Files\Epic Games\UE_5.6\Engine\Build\BatchFiles\RunUAT.bat"
 set PluginPath=%~dp0\OpenAI.uplugin
 set BuildPath=%~dp0\Build
 set "ZipPath=c:\Program Files\7-Zip\7z.exe"
 
 :: package plugin
-"%UATPath%" BuildPlugin -Plugin="%PluginPath%" -Package="%BuildPath%"
+call "%UATPath%" BuildPlugin -Plugin="%PluginPath%" -Package="%BuildPath%"
 
 :: update uplugin file
 python plugin_descriptor_updater.py "%BuildPath%\OpenAI.uplugin"
 
 :: zip
 cd "%BuildPath%"
-"%ZipPath%" a -tzip "build.zip"
+"%ZipPath%" a -tzip "OpenAI.zip"
 cd ..
